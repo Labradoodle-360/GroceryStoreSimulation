0e3063697ae79987ce9b132e50182ad84f5b8189
Matthew "Labradoodle-360" Kerle <creativewebdsign@gmail.com>
2015-12-05 20:42:48 -0600

    Person Class

---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GroceryStoreSimulation
+{
+	public class Person
+	{
+		public int arriveMinute, shoppingTime, timeInLine;
+		public double amountSpent;
+
+		public string state;
+
+		public Person (int tempArriveMinute, int tempShoppingTime, int tempAmountSpent)
+		{
+			arriveMinute = tempArriveMinute;
+			shoppingTime = tempShoppingTime;
+			amountSpent  = tempAmountSpent;
+			timeInLine   = 0;
+			state        = "outside";
+		}
+	}
+}
+
