b0acbb1ccc2e123f95aec1a89dfba2ab007f5197
Noah Henry <201106071@panthers.greenville.edu>
2015-12-05 20:40:24 -0600

    Adding Start Menu
    Get Big, son!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
 
 namespace GroceryStoreSimulation
 {
@@ -6,7 +9,32 @@ namespace GroceryStoreSimulation
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Application.Run (new MyForm ());
+		}
+	}
+
+	class Store : Form
+	{
+		public TextBox t;
+		public MyForm()
+		{
+
+			t          = new TextBox ();
+			t.Width    = 300;
+			t.Location = new Point (10, 10);
+			t.Height   = 40;
+			t.Enabled  = false;
+			t.ReadOnly = true;
+
+			Controls.Add (t);
+
+			MenuItem open = new MenuItem ("Start Simulation");
+			open.Click += Open_Click;
+
+			MainMenu bar = new MainMenu ();
+			bar.MenuItems.Add (file);
+
+			Menu = bar;
 		}
 	}
 }
